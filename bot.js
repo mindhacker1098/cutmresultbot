62be830eb1fa6df7fd6906ec684386e28bf4bfd6
mindhacker <mindhacker1098@gmail.com>
2023-01-17 18:46:15 +0530

    second

--- bot.js
+++ bot.js
@@ -4,26 +4,7 @@ const token = '5988039733:AAF8c1FxKpfnrIIQEf-Q9cZnAwetlk00YGo';
 
 
 const bot = new TelegramBot(token,{polling:true});
-async function ducky(reg,name){
 
-
-    let d=await fetch('https://resultchecker.onrender.com/ducky', {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({reg:reg, name:undefined})
-    })
-    
-    let data=await d.json();
-   let s= await bot.sendMessage(name,data[0].Name)
-    s=await bot.sendMessage(name,data[0]["Registration No"])
-for(let i=0;i<data.length-1;i++){
-    
-s=await bot.sendMessage(name,`${data[i]["Subject Name"]}---->${data[i]["Grade"]}`)}
-bot.sendMessage(name,data[data.length-1].resul)
-}
 
 
 
@@ -31,7 +12,7 @@ bot.onText(/\/start/,(msg,match)=>{
 
 
 
-    bot.sendMessage(msg.chat.id,"Enter your Registration Number if want to know your result")
+    bot.sendMessage(msg.chat.id,`Enter your Registration Number if want to know your result`)
 })
 
 
@@ -40,7 +21,57 @@ bot.on('message', (msg) => {
   const chatId = msg.chat.id;
 console.log(msg.text)
 
-ducky(msg.text,chatId)
+
+
+let s="";
+
+
+fetch('https://resultchecker.onrender.com/ducky', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({reg:msg.text, name:undefined})
+    }).then(res=>res.json()).then(data=>{
+
+
+for(let i=0;i<data.length-1;i++){
+
+if(i==0){s+=`${data[0]["Name"]}
+
+`
+s+=`${data[0]["Registration No"]}
+
+
+`
+
+}
+
+
+  s+=`${data[i]["Subject Name"]}---->${data[i]["Grade"]}
+  
+  
+`
+}
+s+=`${data[data.length-1].resul}`
+bot.sendMessage(chatId,s)
+
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 });
