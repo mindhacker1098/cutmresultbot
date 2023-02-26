f497ee1d0426f40fca84b0aac5a00e4fbef13c1e
mindhacker <mindhacker1098@gmail.com>
2023-02-26 19:12:48 +0530

    changes

--- bot.js
+++ bot.js
@@ -13,42 +13,42 @@ console.log(msg.text)
 
 
 
-// let s="";
+let s="";
 
 
-// fetch('https://resultchecker.onrender.com/ducky', {
-//         method: 'POST',
-//         headers: {
-//           'Accept': 'application/json',
-//           'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify({reg:msg.text, name:undefined})
-//     }).then(res=>res.json()).then(data=>{
+fetch('https://resultchecker.onrender.com/ducky', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({reg:msg.text, name:undefined})
+    }).then(res=>res.json()).then(data=>{
 
 
-// for(let i=0;i<data.length-1;i++){
+for(let i=0;i<data.length-1;i++){
 
-// if(i==0){s+=`${data[0]["Name"]}
+if(i==0){s+=`${data[0]["Name"]}
 
-// `
-// s+=`${data[0]["Registration No"]}
-
-
-// `
-
-// }
+`
+s+=`${data[0]["Registration No"]}
 
 
-//   s+=`${data[i]["Subject Name"]}---->${data[i]["Grade"]}
+`
+
+}
+
+
+  s+=`${data[i]["Subject Name"]}---->${data[i]["Grade"]}
   
   
-// `
-// }
-// s+=`${data[data.length-1].resul}`
-// console.log(s)
-bot.sendMessage(chatId,msg.text)
+`
+}
+s+=`${data[data.length-1].resul}`
+console.log(s)
+bot.sendMessage(chatId,s)
 
-    //})
+    })
 
 
 
